7042a66f6cbcac83373a1bd71f12209b33cf4835
DimaTivator <andriianov164@gmail.com>
2022-10-29 01:34:25 +0300

    aboba

--- Lab4/lab4_task1.py
+++ Lab4/lab4_task1.py
@@ -1,3 +1,79 @@
+def make_text(tag: str) -> str:
+    return tag.replace('<', '').replace('>', '')
+
+
+def make_beautiful_xml(xml_file: str):
+    xml_obj = open(xml_file)
+    result_xml = open('beautiful_xml.xml', 'w')
+
+    xml_list = []
+    for line in xml_obj:
+        xml_list.append(''.join([symbol for symbol in line if symbol not in ['\n', '\t']]))
+
+    result_list = []
+
+    for line in xml_list:
+
+        is_open_tag = False
+        is_close_tag = False
+        open_tag = ''
+        close_tag = ''
+        text = ''
+
+        for i in range(len(line)):
+
+            if is_open_tag:
+                open_tag += line[i]
+
+            if is_close_tag:
+                close_tag += line[i]
+
+            if line[i] == '<':
+                if line[i + 1] == '/':
+                    is_close_tag = True
+                    close_tag += line[i]
+                else:
+                    is_open_tag = True
+                    open_tag += line[i]
+
+                if len(text):
+                    result_list.append(text + '\n')
+                    text = ''
+
+            if not is_open_tag and not is_close_tag:
+                text += line[i]
+
+            if line[i] == '>':
+                if is_close_tag:
+                    is_close_tag = False
+                    result_list.append(close_tag + '\n')
+                    close_tag = ''
+                else:
+                    is_open_tag = False
+                    result_list.append(open_tag + '\n')
+                    open_tag = ''
+
+            if i == len(line) - 1 and len(text):
+                result_list.append(text + '\n')
+
+    indent = -1
+    result_xml.write(result_list[0])
+
+    for i in range(1, len(result_list)):
+
+        if '<' in result_list[i] and '</' not in result_list[i]:
+            if '</' not in result_list[i - 1]:
+                indent += 1
+
+        elif '</' in result_list[i]:
+            indent -= 1
+
+        else:
+            indent += 1
+
+        result_xml.write(indent * '\t' + result_list[i])
+
+
 def xml_to_json_task1(xml_file: str):
     res = open('result.json', 'w')
     xml_obj = open(xml_file)
@@ -17,9 +93,9 @@ def xml_to_json_task1(xml_file: str):
         if '<' in s and '</' not in s:
             tags_stack.append(s)
             if '<' not in xml_list[i + 1]:
-                res.write(indent * ' ' + '"' + s.replace('<', '').replace('>', '') + '": ')
+                res.write(indent * ' ' + '"' + make_text(s) + '": ')
             else:
-                res.write(indent * ' ' + '"' + s.replace('<', '').replace('>', '') + '": {\n')
+                res.write(indent * ' ' + '"' + make_text(s) + '": {\n')
 
         elif '</' in s:
             tags_stack.pop()
@@ -41,4 +117,5 @@ def xml_to_json_task1(xml_file: str):
     xml_obj.close()
 
 
-xml_to_json_task1("timetable.xml")
+make_beautiful_xml("timetable.xml")
+xml_to_json_task1("beautiful_xml.xml")
--- Lab4/lab4_task3.py
+++ Lab4/lab4_task3.py
@@ -1,6 +1,83 @@
 import re
 
 
+def make_text(tag: str) -> str:
+    return tag.replace('<', '').replace('>', '')
+
+
+def make_beautiful_xml(xml_file: str):
+    xml_obj = open(xml_file)
+    result_xml = open('beautiful_xml.xml', 'w')
+
+    xml_list = []
+    for line in xml_obj:
+        xml_list.append(''.join([symbol for symbol in line if symbol not in ['\n', '\t']]))
+
+    result_list = []
+
+    for line in xml_list:
+
+        is_open_tag = False
+        is_close_tag = False
+        open_tag = ''
+        close_tag = ''
+        text = ''
+
+        for i in range(len(line)):
+
+            if is_open_tag:
+                open_tag += line[i]
+
+            if is_close_tag:
+                close_tag += line[i]
+
+            if line[i] == '<':
+                if line[i + 1] == '/':
+                    is_close_tag = True
+                    close_tag += line[i]
+                else:
+                    is_open_tag = True
+                    open_tag += line[i]
+
+                if len(text):
+                    result_list.append(text + '\n')
+                    text = ''
+
+            if not is_open_tag and not is_close_tag:
+                text += line[i]
+
+            if line[i] == '>':
+                if is_close_tag:
+                    is_close_tag = False
+                    result_list.append(close_tag + '\n')
+                    close_tag = ''
+                else:
+                    is_open_tag = False
+                    result_list.append(open_tag + '\n')
+                    open_tag = ''
+
+            if i == len(line) - 1 and len(text):
+                result_list.append(text + '\n')
+
+    indent = -1
+    result_xml.write(result_list[0])
+
+    for i in range(1, len(result_list)):
+
+        # if '<' in result_list[i] and '</' not in result_list[i]:
+        if '<' in result_list[i] and '</' not in result_list[i]:
+            if '</' not in result_list[i - 1]:
+                indent += 1
+
+        elif '</' in result_list[i]:
+            indent -= 1
+
+        else:
+            indent += 1
+
+        result_xml.write(indent * '\t' + result_list[i])
+
+
 def xml_to_json_task3(xml_file: str):
     res = open('result3.json', 'w')
     xml_obj = open(xml_file)
@@ -24,9 +101,9 @@ def xml_to_json_task3(xml_file: str):
         if len(re.findall(open_tag, s)):
             tags_stack.append(s)
             if len(re.findall(open_tag, xml_list[i + 1])) == 0:
-                res.write(indent * ' ' + '"' + s.replace('<', '').replace('>', '') + '": ')
+                res.write(indent * ' ' + '"' + make_text(s) + '": ')
             else:
-                res.write(indent * ' ' + '"' + s.replace('<', '').replace('>', '') + '": {\n')
+                res.write(indent * ' ' + '"' + make_text(s) + '": {\n')
 
         elif len(re.findall(close_tag, s)):
             tags_stack.pop()
@@ -48,4 +125,7 @@ def xml_to_json_task3(xml_file: str):
     xml_obj.close()
 
 
-xml_to_json_task3("timetable.xml")
+make_beautiful_xml("timetable.xml")
+xml_to_json_task3("beautiful_xml.xml")
+
+
